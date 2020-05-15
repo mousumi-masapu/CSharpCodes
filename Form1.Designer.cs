c5eb466229b72912fb04841d31e0a4883aaf9e6d
mousumi-masapu <mousumi.masapu@gmail.com>
2020-05-16 00:46:54 +0200

    Weather Forecast First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WeatherDisplayProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtWeather = new System.Windows.Forms.TextBox();
+            this.txtMinTemp = new System.Windows.Forms.TextBox();
+            this.txtMaxTemp = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MaxTemp(Kelvin)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MinTemp(Kelvin)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "WeatherType";
+            // 
+            // txtWeather
+            // 
+            this.txtWeather.Location = new System.Drawing.Point(244, 295);
+            this.txtWeather.Name = "txtWeather";
+            this.txtWeather.Size = new System.Drawing.Size(175, 26);
+            this.txtWeather.TabIndex = 5;
+            // 
+            // txtMinTemp
+            // 
+            this.txtMinTemp.Location = new System.Drawing.Point(244, 229);
+            this.txtMinTemp.Name = "txtMinTemp";
+            this.txtMinTemp.Size = new System.Drawing.Size(175, 26);
+            this.txtMinTemp.TabIndex = 6;
+            // 
+            // txtMaxTemp
+            // 
+            this.txtMaxTemp.Location = new System.Drawing.Point(244, 169);
+            this.txtMaxTemp.Name = "txtMaxTemp";
+            this.txtMaxTemp.Size = new System.Drawing.Size(175, 26);
+            this.txtMaxTemp.TabIndex = 7;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(244, 39);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(175, 26);
+            this.txtCity.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(244, 104);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(175, 26);
+            this.txtCountry.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Country";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtMaxTemp);
+            this.Controls.Add(this.txtMinTemp);
+            this.Controls.Add(this.txtWeather);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WeatherAPI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWeather;
+        private System.Windows.Forms.TextBox txtMinTemp;
+        private System.Windows.Forms.TextBox txtMaxTemp;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Net;
+
+
+
+namespace WeatherDisplayProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string city;
+            city = txtCity.Text;
+
+            var uri = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&mode=xml&appid=18e45e3c316106ddd1654970d4ae5cbc", city);
+
+            XDocument doc = XDocument.Load(uri);
+            WebClient client = new WebClient();
+
+            //string maxTemp = (string)doc.Descendants("temperature").FirstOrDefault();
+            string maxTemp=(string)doc.Descendants("temperature").FirstOrDefault().Attribute("max").Value;
+            string minTemp = (string)doc.Descendants("temperature").FirstOrDefault().Attribute("min").Value;
+            string weatherType = (string)doc.Descendants("weather").FirstOrDefault().Attribute("value").Value;
+            string country = (string)doc.Descendants("country").FirstOrDefault();
+
+
+            txtMaxTemp.Text = maxTemp;
+            txtMinTemp.Text = minTemp;
+            txtWeather.Text=weatherType;
+            txtCountry.Text = country;
+
+        }
+    }
+}
